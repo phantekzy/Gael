4b464e895b243105740e7de581fdfe7632f1712d
phantekzy <mainilotfi@gmail.com>
2026-04-13 18:52:05 +0100

    now for docx files

--- src/core/extractor.ts
+++ src/core/extractor.ts
@@ -1,11 +1,19 @@
 import pdf from "pdf-parse";
 import { Buffer } from "node:buffer";
+import mammoth from "mammoth";
 export class Extractor {
   static async toText(buffer: Buffer, mimetype: string): Promise<string> {
     try {
       if (mimetype === "application/pdf") {
         const data = await pdf(buffer);
         return data.text;
+      }
+      if (
+        mimetype ===
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      ) {
+        const { value } = await mammoth.extractRawText({ buffer });
+        return value;
       }
     } catch (error) {}
   }
