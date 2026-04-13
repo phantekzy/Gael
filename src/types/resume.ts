2fc3d891f32a95fc9453efd4dce2ca093674abcf
phantekzy <mainilotfi@gmail.com>
2026-04-13 17:19:46 +0100

    Contact & Online Presence

--- src/types/resume.ts
+++ src/types/resume.ts
@@ -13,4 +13,21 @@ export const ResumeSchema = z.object({
     middleName: z.string().trim().optional(),
     title: z.string().trim().optional(),
   }),
+  contact: z.object({
+    email: z.string().email().toLowerCase().trim(),
+    phone: z.string().trim().nullable(),
+    location: z
+      .object({
+        city: z.string().trim().nullable(),
+        country: z.string().trim().nullable(),
+        remoteWork: z.boolean().default(true),
+      })
+      .nullable(),
+    links: z.object({
+      linkedin: z.string().url().nullable(),
+      github: z.string().url().nullable(),
+      portfolio: z.string().url().nullable(),
+      other: z.array(z.string().url()).default([]),
+    }),
+  }),
 });
