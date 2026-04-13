0a9c9923ff5bb9165ee52e5dde0cd3093a96947f
phantekzy <mainilotfi@gmail.com>
2026-04-13 19:03:00 +0100

    file text extraction and sanitization are DOOONE yes BABY

--- src/core/sanitizer.ts
+++ src/core/sanitizer.ts
@@ -1,1 +1,9 @@
-export class Sanitizer {}
+export class Sanitizer {
+  static clean(text: string): string {
+    return text
+      .replace(/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F]/g, "")
+      .replace(/\s\s+/g, " ")
+      .replace(/\n\s*\n/g, "\n")
+      .trim();
+  }
+}
