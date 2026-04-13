eb8430405eea02d61c413bb10992b7f1845b6341
phantekzy <mainilotfi@gmail.com>
2026-04-13 20:01:10 +0100

    -spliting the text based of the spaces and verify the empty lines - added a basic extraction heuristics

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -3,5 +3,16 @@ import { Sanitizer } from "./sanitizer.js";
 export class ResumeParser {
   static parse(rawText: string) {
     const text = Sanitizer.clean(rawText);
+    const lines = text.split("\n").filter((l) => l.length > 0);
+
+    const email =
+      text
+        .match(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)?.[0]
+        ?.toLowerCase() || "phantekzy@email.com";
+
+    const phone =
+      text.match(
+        /(\+?\d{1,3}[-.\s]?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}/,
+      )?.[0] || null;
   }
 }
