df58438bdc347635bcb729c30a0ae4324df962fa
phantekzy <mainilotfi@gmail.com>
2026-04-15 19:37:21 +0100

    Service done

--- src/resume.service.ts
+++ src/resume.service.ts
@@ -1,9 +1,24 @@
-import fs from "fs";
+import fs from "fs/promises";
 import { Extractor } from "./core/extractor.js";
+import { ResumeParser } from "./core/parser.js";
+import { ResumeSchema } from "./types/resume.js";
 
 export class ResumeService {
   static async process(filePath: string, mimeType: string) {
     const buffer = await fs.readFile(filePath);
+
     const rawText = await Extractor.toText(buffer, mimeType);
+
+    const parsedData = ResumeParser.parse(rawText);
+
+    const finalPayload = {
+      ...parsedData,
+      experience: [],
+      education: [],
+      projects: [],
+      certifications: [],
+    };
+
+    return ResumeSchema.parse(finalPayload);
   }
 }
