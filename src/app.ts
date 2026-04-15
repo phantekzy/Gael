0c57debbd43f2216e05a2162d5da02cf7f295f8a
phantekzy <mainilotfi@gmail.com>
2026-04-15 19:15:26 +0100

    Configuring the Storage

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import fs from "fs";
+import multer from "multer";
 
 const app = express();
 
 if (!fs.existsSync("uploads")) {
   fs.mkdirSync("uploads");
 }
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+});
