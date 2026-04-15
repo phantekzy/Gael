31d1f697ae43bb08f27cbaff3f538590b50bf511
phantekzy <mainilotfi@gmail.com>
2026-04-15 19:46:34 +0100

    givving the the heavy lifting to the Service

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
+import { Request, Response } from "express";
 import { upload } from "./app.js";
+import { ResumeService } from "./resume.service.js";
 
-app.post("api/parse", upload.single("resume"), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-  } catch (error) {}
-});
+app.post(
+  "api/parse",
+  upload.single("resume"),
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
+      }
+      const validatedData = await ResumeService.process(
+        req.file.path,
+        req.file.mimetype,
+      );
+    } catch (error) {}
+  },
+);
