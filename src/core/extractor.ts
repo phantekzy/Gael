9cd1c64a61574a6aa230f7a7957879415a71ffc8
phantekzy <mainilotfi@gmail.com>
2026-04-16 11:40:40 +0100

    PDF to be fully loaded into memory so we can access its properties, like the number of pages

--- src/core/extractor.ts
+++ src/core/extractor.ts
@@ -3,5 +3,7 @@ import * as pdfjs from "pdfjs-dist";
 export class Extractor {
   static async toText(buffer: Uint8Array): Promise<string> {
     const loadingTask = pdfjs.getDocument({ data: buffer });
+    const pdf = await loadingTask.promise;
+    let fullText = "";
   }
 }
